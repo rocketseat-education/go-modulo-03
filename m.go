3f60b11ac5913b6e38a392aadd67114984efba4d
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-05-20 16:35:55 -0300

    JSON GET Request

--- m.go
+++ m.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
-	"time"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+type User struct {
+	Username string
+	ID       int64 `json:",string"`
+	Role     string
+	Password string `json:"-"`
+}
 
 func main() {
 	r := chi.NewMux()
@@ -16,37 +23,52 @@ func main() {
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 
-	r.Get("/horario", func(w http.ResponseWriter, r *http.Request) {
-		now := time.Now()
-		fmt.Fprintln(w, now)
-	})
+	db := map[int64]User{
+		1: {
+			Username: "admin",
+			Password: "admin",
+			Role:     "admin",
+			ID:       1,
+		},
+	}
 
-	r.Route("/api", func(r chi.Router) {
-		r.Route("/v1", func(r chi.Router) {
-			r.Get("/users", func(w http.ResponseWriter, r *http.Request) {})
-		})
-
-		r.Route("/v2", func(r chi.Router) {
-		})
-
-		r.With(middleware.RealIP).
-			Get("/users/{id:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
-				id := chi.URLParam(r, "id")
-				fmt.Fprintln(w, id)
-			})
-
-		r.Group(func(r chi.Router) {
-			r.Use(middleware.BasicAuth("", map[string]string{
-				"admin": "admin",
-			}))
-
-			r.Get("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintln(w, "ping")
-			})
-		})
+	r.Group(func(r chi.Router) {
+		r.Use(jsonMiddleware)
+		r.Get("/users/{id:[0-9]+}", handleGetUsers(db))
+		r.Post("/users", handlePostUsers)
 	})
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		panic(err)
 	}
+}
+
+func jsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+func handleGetUsers(db map[int64]User) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		idStr := chi.URLParam(r, "id")
+		id, _ := strconv.ParseInt(idStr, 10, 64)
+
+		user, ok := db[id]
+
+		if ok {
+			data, err := json.Marshal(user)
+			if err != nil {
+				panic(err)
+			}
+
+			_, _ = w.Write(data)
+		}
+	}
+}
+
+func handlePostUsers(w http.ResponseWriter, r *http.Request) {
+
 }

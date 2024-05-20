4522dd881927a74964a4c075d8a2e1f373f5ceef
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-05-20 16:08:19 -0300

    Conhecendo as libs

--- m.go
+++ m.go
@@ -1,43 +1,4 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-	"net/http"
-	"time"
-)
-
-func Log(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		begin := time.Now()
-		next.ServeHTTP(w, r)
-		fmt.Println(r.URL.String(), r.Method, time.Since(begin))
-	})
-}
-
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc(
-		"/api/users/{foo}",
-		func(w http.ResponseWriter, r *http.Request) {
-			id := r.PathValue("foo")
-			fmt.Println(id)
-			fmt.Fprintln(w, "hello, world")
-		},
-	)
-	srv := &http.Server{
-		Addr:                         ":8080",
-		Handler:                      Log(mux),
-		DisableGeneralOptionsHandler: false,
-		ReadTimeout:                  10 * time.Second,
-		WriteTimeout:                 10 * time.Second,
-		IdleTimeout:                  1 * time.Minute,
-	}
-
-	if err := srv.ListenAndServe(); err != nil {
-		if !errors.Is(err, http.ErrServerClosed) {
-			panic(err)
-		}
-	}
 }

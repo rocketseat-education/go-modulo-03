27ded7d478587afee44aa85bff9a4f1a0faab6fd
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-05-21 14:21:10 -0300

    JSON POST Request & Error Handling

--- m.go
+++ m.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
 
@@ -35,7 +38,7 @@ func main() {
 	r.Group(func(r chi.Router) {
 		r.Use(jsonMiddleware)
 		r.Get("/users/{id:[0-9]+}", handleGetUsers(db))
-		r.Post("/users", handlePostUsers)
+		r.Post("/users", handlePostUsers(db))
 	})
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
@@ -58,17 +61,49 @@ func handleGetUsers(db map[int64]User) http.HandlerFunc {
 
 		user, ok := db[id]
 
-		if ok {
-			data, err := json.Marshal(user)
-			if err != nil {
-				panic(err)
-			}
-
-			_, _ = w.Write(data)
+		if !ok {
+			w.WriteHeader(http.StatusNotFound)
+			_, _ = w.Write([]byte(`{"error": "usuario nao encontrado"}`))
+			return
 		}
+
+		data, err := json.Marshal(user)
+
+		if err != nil {
+			http.Error(w, "something went wrong", http.StatusInternalServerError)
+			return
+		}
+
+		_, _ = w.Write(data)
 	}
 }
+func handlePostUsers(db map[int64]User) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.Body = http.MaxBytesReader(w, r.Body, 1000)
 
-func handlePostUsers(w http.ResponseWriter, r *http.Request) {
+		data, err := io.ReadAll(r.Body)
 
+		if err != nil {
+			var maxErr *http.MaxBytesError
+			if errors.As(err, &maxErr) {
+				http.Error(w, "body too large", http.StatusRequestEntityTooLarge)
+				return
+			}
+
+			fmt.Println(err)
+			http.Error(w, "something went wrong", http.StatusInternalServerError)
+			return
+		}
+
+		var user User
+
+		if err := json.Unmarshal(data, &user); err != nil {
+			http.Error(w, "invalid body", http.StatusUnprocessableEntity)
+			return
+		}
+
+		db[user.ID] = user
+
+		w.WriteHeader(http.StatusCreated)
+	}
 }

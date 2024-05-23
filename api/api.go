ab793696be0c5055df7273b5cfa124da0f1db148
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-05-23 14:33:42 -0300

    Projeto 1 - Parte 2

--- api/api.go
+++ api/api.go
@@ -1,21 +1,45 @@
 package api
 
 import (
+	"encoding/json"
+	"log/slog"
+	"math/rand"
 	"net/http"
+	"net/url"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func NewHandler() http.Handler {
+func sendJSON(w http.ResponseWriter, resp Response, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("failed to marshal json data", "error", err)
+		sendJSON(
+			w,
+			Response{Error: "something went wrong"},
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	w.WriteHeader(status)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("failed to write json data", "error", err)
+		return
+	}
+}
+
+func NewHandler(db map[string]string) http.Handler {
 	r := chi.NewMux()
 
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.RequestID)
 	r.Use(middleware.Logger)
 
-	r.Post("/api/shorten", handlePost)
-	r.Get("/{code}", handleGet)
+	r.Post("/api/shorten", handlePost(db))
+	r.Get("/{code}", handleGet(db))
 
 	return r
 }
@@ -29,6 +53,48 @@ type Response struct {
 	Data  any    `json:"data,omitempty"`
 }
 
-func handlePost(w http.ResponseWriter, r *http.Request) {}
+func handlePost(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var body PostBody
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			sendJSON(
+				w,
+				Response{Error: "invalid body"},
+				http.StatusUnprocessableEntity,
+			)
+			return
+		}
 
-func handleGet(w http.ResponseWriter, r *http.Request) {}
+		if _, err := url.Parse(body.URL); err != nil {
+			sendJSON(
+				w,
+				Response{Error: "invalid url passed"},
+				http.StatusBadRequest,
+			)
+		}
+
+		code := genCode()
+		db[code] = body.URL
+
+		sendJSON(
+			w,
+			Response{Data: code},
+			http.StatusCreated,
+		)
+	}
+}
+
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+func genCode() string {
+	const n = 8
+	byts := make([]byte, n)
+	for i := range n {
+		byts[i] = characters[rand.Intn(len(characters))]
+	}
+	return string(byts)
+}
+
+func handleGet(db map[string]string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {}
+}
